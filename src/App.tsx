2bcc6054740fc2b5860e37683fd61606d7394e9e
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-02-11 09:19:19 +0100

    Import ScrollToTop component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 
+import ScrollToTop from "./components/ScrollToTop";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import WatermarkLayer from "./components/WatermarkLayer";
