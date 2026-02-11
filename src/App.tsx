cd8c2ff64fb046c8f62dfd4fdd4ae76a587888b7
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-02-11 09:24:01 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 
 import ScrollToTop from "./components/ScrollToTop";
